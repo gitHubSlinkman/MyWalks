af9a1512db1b357b9071e44e97c712ebc08910a8
Craig W. Slinkman <cwslinkman@gmail.com>
2020-03-16 20:58:19 -0500

    2020-03-16 20:57
    1. Deleted all *.csv files in data
       directory
    
    2. Update exploreData.r

--- develpoment/explore data.R
+++ develpoment/explore data.R
@@ -4,35 +4,48 @@ library( tidyverse )
 library( lubridate )
 library( readxl )
 
+###############################################################################
+# Read MyWalks data files which are storded in xlsx format,
+###############################################################################
+
+read_MyWalks <- function( folder_path, data_file ){
+
+    data_file  <- paste0( data_file,".xlsx")
+    file_path <- file.path( folder_path,
+                             data_file  )
+    read_xlsx( file_path )
+}
 
 FOLDER <- "data"
-DAYS   <- "days.xlsx"
-days_path <- file.path( FOLDER, DAYS )
+DAYS   <- "days"
+ROUTES <- "routes"
 
-days <- read_xlsx( days_path )
+days   <- read_MyWalks( FOLDER, DAYS )
+routes <- read_MyWalks( "data", ROUTES )
 
-str( days )
+days <- left_join( days,
+                   routes,
+                   by = "route" )
 
 ##############################################################################
 # Get year, month, and day from unput date
 ##############################################################################
 
+tz( days$date ) <- "America/Chicago"
+
+days$date      <- as.Date( days$date )
 days$year      <- year( days$date )
 days$month     <- month( days$date )
 days$month_day <- day( days$date )
 
 ###############################################################################
-# We round the srart time to the nearest hour so we can draw a bar cahrt of the
-# starting hours of the walks rounded to the nearest hour.                     
+# We convert the start time to decimal so we can plot a histogram of the start
+# times ...
 ###############################################################################
 
-days$hour   <- hour( days$start_time ) 
-days$minute <- minute( days$start_time )
+days$start_time   <- hour( days$start_time ) +
+                     round( minute( days$start_time )/60, 2)
 
-days$start_hour <- ifelse( days$minute > 20, 
-                           days$hour + 1, 
-                           days$hour )
-days$date   <- as.Date( days$date )
 
 ##############################################################################
 # We transform variables for ease of printing.  ksteps is a thousand steps 
@@ -41,21 +54,36 @@ days$date   <- as.Date( days$date )
 
 days$ksteps <- days$steps / 1000
 
-    days %>% 
+modified_days <- 
+   days %>%
         select( id,
                 date,
                 year:month_day,
-                
                 walked,
                 missed_reason,
-                condition:,
+                weather,
+                temp,
+                rh,
                 shoes,
                 route,
-                start_hour,
+                environment,
+                start_time,
                 miles,
                 -steps,
                 ksteps,
-                hours:failure_reason )
-               
- str(days)   
-
+                miles, 
+                hours, 
+                mph,
+                laps,
+                mpl,
+                pace, 
+                gain, 
+                kcal,
+                avg_hr,
+                max_hr,
+                pace.01:pace.08,
+                breaks,
+                shuffles,
+                device:failure_reason )
+#
+````
