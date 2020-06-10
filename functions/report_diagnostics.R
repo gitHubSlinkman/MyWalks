add6bd0c436730374d66ccee22394cfdf14704b5
Craig Slinkman <cwslinkman@gmail.com>
2020-06-09 20:51:08 -0500

    2020-06-09 8:50
    1. Add 010_categorical_analysis.Rmd
    2. Modified functions_test_bed
    3. Update data/days.xlsx
    4. Updated data/routes.xlsx

--- functions/report_diagnostics.R
+++ functions/report_diagnostics.R
@@ -20,53 +20,7 @@
 #' report_diagbistics( fit_model, diagonstics )
 #' 
 report_diagnostics <- 
-    function( fitted_model, daignostic ){
-        
-        ####################################################################
-        # We need the sample size to set the criteria computed below.
-        ####################################################################
-        
-        n <- dim( diagnostics )[1]
-        p    <- length(fitted_model[[1]])
-        df   <- n - p
-        prob  <-  1 - 0.01 / (2 * n)
-        t_cv   <- rep( prob, n)
-        
-        
-                
-        
-        ####################################################################
-        # Compute and
-        
-        
-        ####################################################################
-        # The compute outlier criteria.
-        ####################################################################
-        
-        RSTUDENT <-                       # Bonferooni critical value
-            qt( prob, df,                  # For outliers 
-                lower.tail = TRUE )
-        DFFITS   <- 3 / sqrt( n )         # Criteria for for deletion
-                                          # fit statistic ...
-        D2       <- 9 / n                 # Criteria for Cook's distance
-                                          # measure ...
-        
-        
-        ####################################################################
-        # Criteria for influenttial observations ...
-        ####################################################################
-        
-        
-        HATVALUES  <-  3.00 * sqrt( p / n  ) # Criteria for high hat
-                                             # values ...
-        L_COVRATIO <-  1.00 - 3 * p / n      # Criteria for low covariance
-                                             # ratio values ...
-        H_COVRATIO <-  1.00 + 3 * p / n      # Criteria for high covariance
-                                             # values ...
-        
-        ###################################################################
-        # We produce the report as a flextable object.
-        ###################################################################
+    function( fitted_model, daignostics ){
         
         diagnostics %>% 
             flextable() %>% 
@@ -79,17 +33,5 @@ report_diagnostics <-
                                covratio = "Covariance\nratio",
                                dffits   = "DFFITS",
                                d2       = "Cooks's\ndistance") %>% 
-            align( align = "left", 
-                   part = "header" ) %>%
-            color( ~ abs( rstudent ) > 3.00,
-                   ~ rstudent , color = "red" ) %>%  
-            bold( ~ abs( rstudent ) > 3.00,
-                  ~ rstudent, bold = TRUE ) %>% 
-            color( ~ hats > 3 * sqrt(p/n),
-                   ~ hats, color="orange" ) %>% 
-            bold( ~ covratio < L_COVRATIO,
-                  ~ covratio, bold = TRUE )
-        
-            
-        
+            autofit()
     }
