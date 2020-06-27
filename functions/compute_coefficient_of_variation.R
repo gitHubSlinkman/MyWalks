9cea72ee0d53832b03cc9b1a6a0f868bc5c5b6ad
Craig Slinkman <cwslinkman@gmail.com>
2020-06-26 20:05:22 -0500

    2020-06-27 10:03
    1. Modified functions/compute_coefficient_of_variation.R

--- functions/compute_coefficient_of_variation.R
+++ functions/compute_coefficient_of_variation.R
@@ -10,18 +10,18 @@ compute_coefficient_of_variation <-
     function( means, stds  ){
         
         label = "Cofficient of variation:"   
-        walked = NA
-        miles  = stds$miles / means$miles
-        steps  = stds$steps / means$steps
-        hours  = stds$hours / means$hours
-        mph    = stds$mph /  means$mph
-        pace   = stds$pace / means$pace
-        gain   = stds$ gain / means$gain
-        kcal   = stds$kcal / means$kcal
-        avg_hr = stds$avg_hr / means$avg_hr
-        max_hr = stds$max_hr / means$max_hr
-        breaks = stds$breaks / means$breaks
-        shuffles = stds$shuffles / means$shuffles
+        walked   = NA
+        miles    = 100 * stds$miles / means$miles
+        steps    = 100 * stds$steps / means$steps
+        hours    = 100 * stds$hours / means$hours
+        mph      = 100 * stds$mph /  means$mph
+        pace     = 100 * stds$pace / means$pace
+        gain     = 100 * stds$ gain / means$gain
+        kcal     = 100 * stds$kcal / means$kcal
+        avg_hr   = 100 * stds$avg_hr / means$avg_hr
+        max_hr   = 100 * stds$max_hr / means$max_hr
+        breaks   = 100 * stds$breaks / means$breaks
+        shuffles = 100 * stds$shuffles / means$shuffles
         
         tibble( label,
                 walked,
