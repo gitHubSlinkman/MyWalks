06117d2fd8bd2f616f90c32e2cbc815f67f8677c
Craig Slinkman <cwslinkman@gmail.com>
2020-07-03 23:57:58 -0500

    2020-07-03 23:56
    1. Modified 0020_report_last_007_days_walks.Rmd
    2. add functions/summarise_zones.R

---
+++ functions/summarise_zones.R
@@ -0,0 +1,55 @@
+# summarize_zones.R
+
+summarize_zones <- 
+    function( walks ){
+        
+        ######################################################################
+        # Extract variable  from walks.
+        ######################################################################
+        
+        tbl_temp <- 
+            walks %>%
+                select( id,
+                        date_time,
+                        route,
+                        zone1:zone5 ) %>% 
+                    filter( complete.cases(.))
+        
+        
+        ###################################################################
+        # Pivot on zomes to grate longer form of the data.
+        ###################################################################
+        
+        longer_tbl <- 
+            pivot_longer( tbl_temp,
+                          cols = starts_with("zone"),
+                          names_to = "zone",
+                          values_to = "minutes",
+                          values_drop_na = TRUE )
+        
+        
+        ####################################################################
+        # Summarize the hours walked in each zone and pass the the
+        # summary tibble back to the the calling script.
+        ####################################################################
+        
+        summary <- 
+            longer_tbl %>% 
+                group_by( zone ) %>% 
+                summarise( hours = sum( minutes ) / 60 )
+        
+        zone_label <- 
+            c(  "< 88",
+                "89 - 103",
+                "104 - 118",
+                "119 - 133",
+                "> 130")
+        
+        add_column( summary,
+                    zone_label ) %>% 
+            select(  zone,
+                     zone_label,
+                     hours )
+    }
+
+
