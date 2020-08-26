a7141e87b6ed3a31a8430de991d20307f7211abf
Craig Slinkman <cwSlinkman@gmail.com>
2020-08-26 16:43:48 -0500

    2020-08-25 16:43
    1. Add functions/filter_by_days.R
    2. Add tests/test__filter_by_datys.R

---
+++ functions/filter_by_days.R
@@ -0,0 +1,13 @@
+# filter_by_days.R
+
+require( tidyverse )                    # I live in the tidyverse.
+require( lubridate )                    # For date processing ...
+
+filter_by_days <- 
+    function( walks, days )
+    {
+        current_date <- today()
+        filter_date  <- current_date - days + 1
+        walks %>% 
+            filter( date_time > filter_date )
+    }
