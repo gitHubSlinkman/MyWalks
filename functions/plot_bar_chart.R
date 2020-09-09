2525e35a267e83ce4e02c7eddb980781789bc8eb
Craig Slinkman <cwSlinkman@gmail.com>
2020-09-08 19:38:01 -0500

    2020-09-08 19:36
    1. Debug 0020_report_last_007_days_walks.Rmd
    2. Debug 20_last_7_days_walks.Rmd
    3. Added days walks to data/days.xlsx
    4. Debugging functions/plot_bar_cbart.R
    5. Created test__plot_bar_chart.R

--- functions/plot_bar_chart.R
+++ functions/plot_bar_chart.R
@@ -1,8 +1,36 @@
 # plot_bar_char.R
+
 ###############################################################################
-# plot_bar_chart( variable)
+# Load required packages.
 ###############################################################################
 
+require(tidyverse)                      # I live in the tidyverse ...
+require(lubridate)                      # For advanced date processing ...
+
+###############################################################################
+# Load required functions
+###############################################################################
+
+source('D:/R-Projects/MyWalks/functions/find_max_frequency.R')
+
+
+#' This function draws a frequency bar-chart of a variable.  If there are
+#' missing variables it removes them prior to drawing the bar-chart.
+#'
+#' @param variable         is the name of the vector to  to be plotted.         - 
+#' @param frequency_scale  is the optional scale to be used for the x-axis,
+#' @param variable_name    is the label of the x-axis.  
+#' @param plot_title       is the plot title 
+#' @param bar_width        used to override default bar width
+#' @param fill_color       used to override  default bar color
+#'
+#' @return                 a plot object
+#' @export                 none
+#' 
+#' @examples               none
+       
+ 
+ 
 plot_bar_chart <- 
     function( variable,
               frequency_scale = NULL,
@@ -19,12 +47,13 @@ plot_bar_chart <-
               variable <- variable[ !is.na( variable )]
               
               ###############################################################
-              # Determine the maximum fequency to be plotted on the bar
-              # so can allocate addtional space to display frquences without
-              # them being partially visoble.
+              # Determine the maximum frequency to be plotted on the bar
+              # so can allocate additional space to display frquences without
+              # them being partially variable.
               ###############################################################
               
                max_frequency <- find_max_frequency( variable )
+               
               
               
               ##############################################################
@@ -32,15 +61,20 @@ plot_bar_chart <-
               # bar chart.
               ##############################################################
         
-              tibble( variable ) %>% 
-                   ggplot( aes( x = variable )) +
-                      geom_bar( color = "black",
-                                fill = "green" ) +
+              temp_tibble <- tibble( variable )
+              
+              ggplot( temp_tibble,
+                      aes( x = Activity )) +
+                geom_bar( )
+               
+               
+               
                       geom_text( stat = 'count',
                                  aes(label =..count.., 
                                      vjust = -0.2)) +
                       expand_limits( y = max_frequency + 10 ) +
                       xlab( variable_name ) +
                       ylab( "Frequency" ) +
-                      ggtitle( plot_title )
+                      ggtitle( plot_title ) +
+                      theme_cowplot()
     }
