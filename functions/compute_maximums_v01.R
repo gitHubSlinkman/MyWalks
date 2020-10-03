230cdfd1047898fa30cc05240d92035660578b9c
Craig Slinkman <cwSlinkman@gmail.com>
2020-10-03 18:00:08 -0500

    2020-10-03 17:58
    1. Added functions/compute_maximums.R
    2. Added tests/test__compute_maximums.Rmd

---
+++ functions/compute_maximums_v01.R
@@ -0,0 +1,21 @@
+# computemaximums.R
+
+require(tidyverse)
+
+compute_maximums <- function( days )
+{
+    days %>% 
+        summarise( label    = "Maximums:",
+                   walked   = NA,
+                   miles    = max( miles,   na.rm = TRUE),
+                   steps    = max( steps,   na.rm = TRUE ),
+                   hours    = max( hours,   na.rm = TRUE ),
+                   mph      = max( mph,     na.rm = TRUE ),
+                   pace     = max( pace,    na.rm = TRUE ),
+                   kcal     = max( kcal,    na.rm = TRUE),
+                   avg_hr   = max( avg_hr,  na.rm = TRUE ),
+                   max_hr   = max( max_hr,  na.rm = TRUE ),
+                   breaks   = max( breaks,  na.rm = TRUE ),
+                   stumbles = max(stumbles, na.rm = TRUE ),
+                   falls    = max( falls,    na.rm = TRUE ))
+} 
