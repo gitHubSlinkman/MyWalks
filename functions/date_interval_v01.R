f5d8e9a95246d1142a118b9a4b3120647a360b38
Craig Slinkman <cwSlinkman@gmail.com>
2020-10-05 20:05:49 -0500

    2020-10-05 20:03
    1. Added walk to data/days.xlsx
    2. Added functions/data_interval_v01.Rmd
    3. Added tests/test__make_date_interval.Rmd

---
+++ functions/date_interval_v01.R
@@ -0,0 +1,24 @@
+# make_date_interval.R
+
+require( tidyverse )
+require( lubridate )
+
+make_date_interval <- 
+    function( day_routes )
+    {
+        start_date_time <- day_routes$date_time[1]
+        end_date_time   <- day_routes$date_time[dim(day_routes)[1] ]
+        interval( start_date_time, end_date_time )
+    }
+
+display_date_interval <- 
+    function( date_interval )
+    {
+       start_dt <- as_date( int_start( date_interval ))
+       end_dt   <- as_date( int_end( date_interval ))  
+       
+       paste0( "For the period from ",
+               start_dt,
+               " to ",
+               end_dt )
+    }
