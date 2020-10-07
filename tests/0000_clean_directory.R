a8e86074bee7b3874c5601ba44b213d7dd7701e1
Craig Slinkman <cwSlinkman@gmail.com>
2020-10-07 16:52:58 -0500

    2020-10-09 16:51
    1. Added walk to data/days.xlsx
    2. Added tests/0000_clean_directory.R

---
+++ tests/0000_clean_directory.R
@@ -0,0 +1,25 @@
+old_directory <- getwd()
+new_directory <- file.path( old_directory, "tests")
+before <- dir()
+before
+unlink("*.html")
+unlink( "010_impute_missing_files", recursive=TRUE )
+unlink( pattern = glob2rx( "*_files" ),
+        recursive=TRUE )
+after <- dir()
+after
+delta <- setdiff( before, after )
+delta
+
+directories <- list.dirs( recursive=FALSE)
+
+pattern <- glob2rx( "*_files" )
+
+library( stringr )
+
+to_delete <- directories[grepl( pattern, directories )]
+
+unlink( to_delete, recursive=TRUE )
+dir()
+setwd( old_directory )
+
