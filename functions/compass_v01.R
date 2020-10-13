ffdff1804f231d71a230200f0f54bf6480bb228a
Craig Slinkman <cwSlinkman@gmail.com>
2020-10-12 19:25:06 -0500

    2020-10-12 19:20
    1. Debugged 0010-plan_walks.Rmd
    2. Deleted data/compass_v01.xlsx
    3. Update functions/ get_day_routes_v01.R
    4. Deleted tests/test__define_compass.Rmd
    5. Deleted tests/test_trst_compass_v01.Rmd

--- functions/compass_v01.R
+++ functions/compass_v01.R
@@ -24,22 +24,22 @@ get_compass <-
 
 
 ###############################################################################
+# Get direction factors from compass.
+###############################################################################
+
+get_levels <- 
+    function( compass )
+    { 
+        compass$level
+    }
+
+
+###############################################################################
 # This function looks use search key to return the row number of the first
 # math in the table.  If the key is not found then a value of zero is returned.
 # There are no rows for CALM and VAR (variable_winds).
 ################################################################################
 
-find_compass_row <- 
-    function( key )
-    {
-    
-        
-        row <- match( key, 
-                      compass$direction,
-                      nomatch = 0 )
-        
-        ifelse( row <= 2, 0, row )
-    }
-    
+
         
     
