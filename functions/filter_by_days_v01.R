328a4d6dca03ccd385c03de82053e0882bd9d654
Craig Slinkman <cwSlinkman@gmail.com>
2020-12-06 21:17:48 -0600

    2020-12-06 21:15
    1. Delelted extract.R
    2. Update data/days.xlsx
    3. Modified functions/filter_by_days.R
    4. Added tests/filter_by_dates.Rmd

--- functions/filter_by_days_v01.R
+++ functions/filter_by_days_v01.R
@@ -6,8 +6,19 @@ require( lubridate )                    # For date processing ...
 filter_by_days <- 
     function( walks, days )
     {
-        current_date <- today()
-        filter_date  <- current_date - days + 1
+        
+        ###################################################################
+        # We date the retrieved data from the last date in the tibble.
+        ###################################################################
+        
+        n                   <- dim( walks )[1]
+        last_walk_date_time <- walks$date_time[n]
+        last_walk_date <- date( last_walk_date_time )
+        
+        
+       
+        filter_date  <- last_walk_date - days 
+        
         walks %>% 
             filter( date_time > filter_date )
     }
